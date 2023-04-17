f8c96b0da7c0779c5eb9b2133f36cad94f600846
JoaoMariaFranco <jmjmf1@iscte-iul.pt>
2023-04-18 00:51:44 +0100

    CloudLint feito (Com problemas menores)

--- src/main/java/pt/iscte_iul/ista/DIAM/SaveCsv.java
+++ src/main/java/pt/iscte_iul/ista/DIAM/SaveCsv.java
@@ -1,4 +1,3 @@
-
 package pt.iscte_iul.ista.DIAM;
 import java.io.*;
 import java.nio.file.Files;
@@ -9,16 +8,19 @@ import org.kohsuke.github.*;
 
 public class SaveCsv{
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException{
 
 		InputStreamReader ir = new InputStreamReader(System.in);
 		BufferedReader in = new BufferedReader(ir);
-		BufferedReader bufferedReader=null;
-		BufferedWriter bufferedWriter=null;
+		BufferedReader bufferedReader = null;
+		BufferedWriter bufferedWriter= null;;
+
 		String  metodo ,OndeGuardar , nome ,DfilePath , SfilePath,donoR,repositorio,token;
 
 		System.out.println("Especifique a Directoria do ficheiro Fonte\n");
+
 		SfilePath=in.readLine();
+
 
 		System.out.println("Nome que pretende dar ao ficheiro\n");
 		nome =in.readLine()+".csv" ;
@@ -29,6 +31,8 @@ public class SaveCsv{
 
 			System.out.println("Directoria onde pretende salvar o ficheiro\n");
 			OndeGuardar = in.readLine();
+
+
 
 
 			DfilePath=OndeGuardar + nome ;
@@ -46,52 +50,62 @@ public class SaveCsv{
 					bufferedWriter.newLine();
 				}
 				System.out.println("Data written to the destination CSV file successfully.");
+			} catch (FileNotFoundException f){
+				f.printStackTrace();
 			}
-			finally
-			{
-				// Close the BufferedReader and BufferedWriter
-				bufferedReader.close();
-				bufferedWriter.close();
+			finally{
+				if(bufferedReader != null && bufferedWriter != null){
+
+					try{
+
+						// Close the BufferedReader and BufferedWriter
+						bufferedReader.close();
+
+						bufferedWriter.close();
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+				}
+
+
+
+
+
+
+
 			}
+			if(metodo.contentEquals("web")){
+
+				// GitHub repository information
+				System.out.println("GitHub Username:\n");
+				donoR = in.readLine();
+				System.out.println("Repositorio GitHub:\n");
+				repositorio = in.readLine();
+
+
+				// GitHub API token for authentication
+				System.out.println("Token de autenticacao GitHub:\n");
+				token = in.readLine();
+
+				// Read the existing CSV file from disk
+				Path path = Paths.get(SfilePath);
+				byte[] fileContent = Files.readAllBytes(path);
+
+				// Authenticate with GitHub using the API token
+				GitHub github = new GitHubBuilder().withOAuthToken(token).build();
+
+				// Get the repository to update the file in
+				GHRepository repository = github.getRepository(donoR + "/" + repositorio);
 
 
 
+				// Create a new file in the repository with the CSV data
+				GHContentBuilder builder = repository.createContent();
+				builder.content(fileContent).message("Added " + nome).path(nome).commit();
 
-
-
-
-		}
-		if(metodo.contentEquals("web")){
-
-			// GitHub repository information
-			System.out.println("GitHub Username:\n");
-			donoR = in.readLine();
-			System.out.println("Repositorio GitHub:\n");
-			repositorio = in.readLine();
-
-
-			// GitHub API token for authentication
-			System.out.println("Token de autenticacao GitHub:\n");
-			token = in.readLine();
-
-			// Read the existing CSV file from disk
-			Path path = Paths.get(SfilePath);
-			byte[] fileContent = Files.readAllBytes(path);
-
-			// Authenticate with GitHub using the API token
-			GitHub github = new GitHubBuilder().withOAuthToken(token).build();
-
-			// Get the repository to update the file in
-			GHRepository repository = github.getRepository(donoR + "/" + repositorio);
-
-
-
-			// Create a new file in the repository with the CSV data
-			GHContentBuilder builder = repository.createContent();
-			builder.content(fileContent).message("Added " + nome).path(nome).commit();
-
-			// Print a message to indicate that the file was uploaded
-			System.out.println("File " + nome + " was uploaded to " + donoR + "/" + repositorio);
+				// Print a message to indicate that the file was uploaded
+				System.out.println("File " + nome + " was uploaded to " + donoR + "/" + repositorio);
+			}
 		}
 	}
 }
--- src/main/java/pt/iscte_iul/ista/DIAM/SaveJson.java
+++ src/main/java/pt/iscte_iul/ista/DIAM/SaveJson.java
@@ -1,4 +1,3 @@
-
 package pt.iscte_iul.ista.DIAM;
 import java.io.*;
 import java.nio.file.Files;
@@ -9,16 +8,19 @@ import org.kohsuke.github.*;
 
 public class SaveJson{
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException{
 
 		InputStreamReader ir = new InputStreamReader(System.in);
 		BufferedReader in = new BufferedReader(ir);
-		BufferedReader bufferedReader=null;
-		BufferedWriter bufferedWriter=null;
+		BufferedReader bufferedReader = null;
+		BufferedWriter bufferedWriter= null;;
+
 		String  metodo ,OndeGuardar , nome ,DfilePath , SfilePath,donoR,repositorio,token;
 
 		System.out.println("Especifique a Directoria do ficheiro Fonte\n");
+
 		SfilePath=in.readLine();
+
 
 		System.out.println("Nome que pretende dar ao ficheiro\n");
 		nome =in.readLine()+".json" ;
@@ -31,6 +33,8 @@ public class SaveJson{
 			OndeGuardar = in.readLine();
 
 
+
+
 			DfilePath=OndeGuardar + nome ;
 
 
@@ -40,58 +44,70 @@ public class SaveJson{
 				bufferedWriter=new BufferedWriter(new FileWriter(DfilePath));
 
 				String line;
-				// Read each line of the source json file and write it to the destination JSON file
+				// Read each line of the source json file and write it to the destination json file
 				while ((line = bufferedReader.readLine()) != null) {
 					bufferedWriter.write(line);
 					bufferedWriter.newLine();
 				}
 				System.out.println("Data written to the destination json file successfully.");
+			} catch (FileNotFoundException f){
+				f.printStackTrace();
 			}
-			finally
-			{
-				// Close the BufferedReader and BufferedWriter
-				bufferedReader.close();
-				bufferedWriter.close();
+			finally{
+				if(bufferedReader != null && bufferedWriter != null){
+
+					try{
+
+						// Close the BufferedReader and BufferedWriter
+						bufferedReader.close();
+
+						bufferedWriter.close();
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+				}
+
+
+
+
+
+
+
 			}
+			if(metodo.contentEquals("web")){
+
+				// GitHub repository information
+				System.out.println("GitHub Username:\n");
+				donoR = in.readLine();
+				System.out.println("Repositorio GitHub:\n");
+				repositorio = in.readLine();
+
+
+				// GitHub API token for authentication
+				System.out.println("Token de autenticacao GitHub:\n");
+				token = in.readLine();
+
+				// Read the existing JSON file from disk
+				Path path = Paths.get(SfilePath);
+				byte[] fileContent = Files.readAllBytes(path);
+
+				// Authenticate with GitHub using the API token
+				GitHub github = new GitHubBuilder().withOAuthToken(token).build();
+
+				// Get the repository to update the file in
+				GHRepository repository = github.getRepository(donoR + "/" + repositorio);
 
 
 
+				// Create a new file in the repository with the JSON data
+				GHContentBuilder builder = repository.createContent();
+				builder.content(fileContent).message("Added " + nome).path(nome).commit();
 
-
-
-
-		}
-		if(metodo.contentEquals("web")){
-
-			// GitHub repository information
-			System.out.println("GitHub Username:\n");
-			donoR = in.readLine();
-			System.out.println("Repositorio GitHub:\n");
-			repositorio = in.readLine();
-
-
-			// GitHub API token for authentication
-			System.out.println("Token de autenticacao GitHub:\n");
-			token = in.readLine();
-
-			// Read the existing JSON file from disk
-			Path path = Paths.get(SfilePath);
-			byte[] fileContent = Files.readAllBytes(path);
-
-			// Authenticate with GitHub using the API token
-			GitHub github = new GitHubBuilder().withOAuthToken(token).build();
-
-			// Get the repository to update the file in
-			GHRepository repository = github.getRepository(donoR + "/" + repositorio);
-
-
-
-			// Create a new file in the repository with the JSON data
-			GHContentBuilder builder = repository.createContent();
-			builder.content(fileContent).message("Added " + nome).path(nome).commit();
-
-			// Print a message to indicate that the file was uploaded
-			System.out.println("File " + nome + " was uploaded to " + donoR + "/" + repositorio);
+				// Print a message to indicate that the file was uploaded
+				System.out.println("File " + nome + " was uploaded to " + donoR + "/" + repositorio);
+			}
 		}
 	}
 }
+
+
