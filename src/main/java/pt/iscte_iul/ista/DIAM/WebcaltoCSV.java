c18fb3a6c3aefa83caefc3e4c853b77c2560a790
DiogoPeng <dbpgo@iscte-iul.pt>
2023-04-24 13:58:13 +0100

    WebtoJson

---
+++ src/main/java/pt/iscte_iul/ista/DIAM/WebcaltoCSV.java
@@ -0,0 +1,14 @@
+package pt.iscte_iul.ista.DIAM;
+
+
+
+
+public class WebcaltoCSV {
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        
+        
+}
+}
---
+++ src/main/java/pt/iscte_iul/ista/DIAM/WebcaltoJson.java
@@ -0,0 +1,36 @@
+package pt.iscte_iul.ista.DIAM;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.net.URL;
+
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import net.fortuna.ical4j.data.CalendarBuilder;
+import net.fortuna.ical4j.data.ParserException;
+
+
+public class WebcaltoJson {
+
+    public void WebcaltoJson1(String stringuri) {
+      try {
+        URL url = new URL(stringuri);
+        CalendarBuilder builder = new CalendarBuilder();
+        net.fortuna.ical4j.model.Calendar calendar = builder.build(url.openStream());
+        ObjectMapper mapper = new ObjectMapper();
+        String json = mapper.writeValueAsString(calendar);
+        FileWriter fw = new FileWriter("output1.json");
+        fw.write(json);
+        fw.close();
+        System.out.println(json);
+
+      } catch (IOException | ParserException e) {
+        e.printStackTrace();
+      }
+    }
+}
+
+   
+
+    
