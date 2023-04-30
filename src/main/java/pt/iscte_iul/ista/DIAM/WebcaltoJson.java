6a7a28c9219b6982fc4d524e3eddd46a5257b0f2
DiogoPeng <dbpgo@iscte-iul.pt>
2023-04-30 16:11:12 +0100

    WEBtoCSV em vez de WEBtoJSON e inicio do ShowCSV

--- src/main/java/pt/iscte_iul/ista/DIAM/WebcaltoJson.java
+++ src/main/java/pt/iscte_iul/ista/DIAM/WebcaltoJson.java
@@ -1,36 +1,55 @@
 package pt.iscte_iul.ista.DIAM;
 
+import java.io.FileInputStream;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.net.URISyntaxException;
 import java.net.URL;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
-
+import org.json.JSONArray;
+import org.json.JSONObject;
+import net.fortuna.ical4j.data.CalendarBuilder;
+import net.fortuna.ical4j.data.ParserException;
+import net.fortuna.ical4j.model.Calendar;
+import net.fortuna.ical4j.model.Component;
+import net.fortuna.ical4j.model.component.VEvent;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVPrinter;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import net.fortuna.ical4j.data.CalendarBuilder;
 import net.fortuna.ical4j.data.ParserException;
 
-
 public class WebcaltoJson {
 
-    public void WebcaltoJson1(String stringuri) {
-      try {
-        URL url = new URL(stringuri);
-        CalendarBuilder builder = new CalendarBuilder();
-        net.fortuna.ical4j.model.Calendar calendar = builder.build(url.openStream());
-        ObjectMapper mapper = new ObjectMapper();
-        String json = mapper.writeValueAsString(calendar);
-        FileWriter fw = new FileWriter("output1.json");
-        fw.write(json);
-        fw.close();
-        System.out.println(json);
+  public static void WebcaltoJson1(String stringuri) throws URISyntaxException, IOException, ParseException {
+    List<CalendarEvent> CEvent = WebcalCalendarImporter.importEventsFromWebcal(stringuri);
+    FileWriter fw = new FileWriter("output4.csv");
+    CSVPrinter csvPrinter = new CSVPrinter(fw, CSVFormat.DEFAULT.withHeader(
+        "Title", "Description", "Start Date", "End Date"));
+    for (CalendarEvent a : CEvent) {
+      List<String> record = new ArrayList<>();
+      record.add(a.getTitle());
+      record.add(a.getDescription());
+      record.add(a.getStartDate().toString());
+      record.add(a.getEndDate().toString());
 
-      } catch (IOException | ParserException e) {
-        e.printStackTrace();
-      }
+      csvPrinter.printRecord(record);
     }
+    csvPrinter.close();
+    fw.close();
+  }
+
+
+  public static void main(String[] args) throws IOException, ParserException, URISyntaxException, ParseException {
+    WebcaltoJson1(
+        "https://fenix.iscte-iul.pt/publico/publicPersonICalendar.do?method=iCalendar&username=dbrlo@iscte.pt&password=V5vG3uiVsWNaNDkKYT3tdP1R392Ma9Gq9l8Fl4QTdlz2Q99UWamZ0rKkRVhac7CwnXxCOimdsXWebkf2aKtqdkU8gn4SrnYicTp45qxidxeaUARk2UVhRlRLtKHcNJ13");
+
+  }
+
 }
-
-   
-
-    
---
+++ src/main/java/pt/iscte_iul/ista/DIAM/showCSV.java
@@ -0,0 +1,16 @@
+package pt.iscte_iul.ista.DIAM;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class showCSV {
+public List<CalendarEvent> showHorario(String path){
+    List<CalendarEvent> horario = new ArrayList<>();
+    if(path.endsWith("csv")){
+        
+    }
+
+return horario;
+}
+
+}
