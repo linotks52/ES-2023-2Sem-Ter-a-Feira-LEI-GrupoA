3cd0a8e3f67ba216b90ea798b47445abf66a7092
jarcv <jarcv@iscte-iul.pt>
2023-05-02 13:31:41 +0100

    atualizar converts

--- src/main/java/Convert/CsvToJson.java
+++ src/main/java/Convert/CsvToJson.java
@@ -3,6 +3,7 @@ package Convert;
 import org.json.CDL;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
@@ -10,17 +11,25 @@ import java.io.InputStreamReader;
 import java.util.stream.Collectors;
 
 public class CsvToJson {
-
-    public static void main(String[] args) {
-        InputStream inputStream = CsvToJson.class.getClassLoader().getResourceAsStream("input.csv");
+    /**
+     *  A funçao converte de csv to json
+     * 
+     * @param  csv file that the user summits 
+     * @return the file converted to json
+     */
+    public FileWriter convert(File f){
+    InputStream inputStream = CsvToJson.class.getClassLoader().getResourceAsStream(f.getAbsolutePath());
         String csvAsString = new BufferedReader(new InputStreamReader(inputStream)).lines().collect(Collectors.joining("\n"));
         String json = CDL.toJSONArray(csvAsString).toString();
+        FileWriter fw;
         try {
-            FileWriter fw = new FileWriter("output.json");
+            fw = new FileWriter("output.json");
             fw.write(json);
             fw.close();
+            return fw;
         } catch (IOException e) {
             e.printStackTrace();
         }
+        return null;
     }
 }
