565ec71ac95b13f13a8278b89e24acff44e994bd
DiogoPeng <dbpgo@iscte-iul.pt>
2023-05-05 20:24:17 +0100

    showCSV2.0(fim)

--- src/main/java/Convert/JsonToCsv.java
+++ src/main/java/Convert/JsonToCsv.java
@@ -2,33 +2,42 @@ package Convert;
 
 import org.json.CDL;
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONTokener;
-
-import com.opencsv.CSVWriter;
 
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.InputStream;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileReader;
 
 public class JsonToCsv {
+
+    public static void main(String[] args) {
+        convert(new File("C:/Users/Utilizador/Desktop/ES/ES-2023-2Sem-Ter-a-Feira-LEI-GrupoA-3/src/main/resources/input.json"));
+    }
  /**
      *  A funçao converte de json to csv
      * 
-     * @param  json file that the user summits 
+     * @param  f file that the user summits 
      * @return the file converted to csv
      */
-    public static FileWriter convert(File f){
-        InputStream inputStream = JsonToCsv.class.getClassLoader().getResourceAsStream(f.getAbsolutePath());
-        JSONArray jsonArray = new JSONArray(new JSONTokener(inputStream));
-        try {
-            FileWriter fw = new FileWriter("output.csv");
-            fw.write(CDL.toString(jsonArray));
-            fw.close();
-            return fw;
-        } catch (IOException e) {
+    static public File convert(File f){
+       
+       // InputStream inputStream = JsonToCsv.class.getClassLoader().getResourceAsStream(f.getAbsolutePath());
+        try { 
+            BufferedReader br = new BufferedReader(new FileReader(f.getAbsolutePath()));
+            JSONArray  jsonArray = new JSONArray(new JSONTokener(br.readLine()));
+            BufferedWriter bw = new BufferedWriter(new FileWriter("input.csv"));
+            bw.write(CDL.toString(jsonArray));
+            bw.close();
+            br.close();
+        } catch (JSONException | IOException e) {
+            // TODO Auto-generated catch block
             e.printStackTrace();
         }
-        return null;
+        return new File("output.csv");
     }
+
 }
