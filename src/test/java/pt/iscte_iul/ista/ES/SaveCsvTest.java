6f0cc6b7e045d6c5b78d3e3055bc37fb0382a840
JoaoMariaFranco <jmjmf1@iscte-iul.pt>
2023-05-06 08:32:45 +0100

    Junit Tests
    Falta Online

--- src/test/java/pt/iscte_iul/ista/ES/SaveCsvTest.java
+++ src/test/java/pt/iscte_iul/ista/ES/SaveCsvTest.java
@@ -1,7 +1,35 @@
 package pt.iscte_iul.ista.ES;
 
-import junit.framework.TestCase;
+import static org.junit.jupiter.api.Assertions.*;
 
-public class SaveCsvTest extends TestCase {
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import org.junit.jupiter.api.Test;
+
+import Save.SaveJson;
+
+class SaveJsonTest {
+
+	@Test
+	public void testSaveOnline() {
+		
+	}
+
+	@Test
+	public void testSaveLocalmente() throws IOException {
+		Path tempDir = Files.createTempDirectory("test");
+		
+		String dFile=tempDir.toString();
+		String sFile = "/ES/OLA.json";
+		SaveJson.saveLocalmente("TESTE",sFile,dFile);
+		
+		assertTrue(Files.exists(tempDir.resolve("TESTE.csv")));
+		
+		Files.delete(tempDir.resolve("TESTE.csv"));
+	    Files.delete(tempDir);
+
+	}
 
 }
