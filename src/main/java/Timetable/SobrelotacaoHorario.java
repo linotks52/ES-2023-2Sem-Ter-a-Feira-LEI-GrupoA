3089d3fec9ce3922c0e973c4e31e1e7eed3c348e
DuarteSL <dslor@iscte-iul.pt>
2023-05-06 04:44:15 +0100

    javadoc

--- src/main/java/Timetable/SobrelotacaoHorario.java
+++ src/main/java/Timetable/SobrelotacaoHorario.java
@@ -6,23 +6,32 @@ import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Classe SobrelotacaoHorario com métodos para identificar eventos sobrelotados e contá-los
+ */
 public class SobrelotacaoHorario {
-    
-    public static List<CalendarEvent> getSobrelotacoes(List<CalendarEvent> input){
+
+    /**
+     * Devolve uma lista de CalendarEvents que estão sobrelotados, ou seja o número de inscritos é maior que a capacidade da sala
+     *
+     * @param input lista de CalendarEvents
+     * @return lista de CalendarEvents sobrelotados
+     */
+    public static List<CalendarEvent> getSobrelotacoes(List<CalendarEvent> input) {
         List<CalendarEvent> lista = new ArrayList<>();
-        for (CalendarEvent e : input){
+        for (CalendarEvent e : input) {
             String s = e.getDescription();
-            System.out.println(s);
+            //System.out.println(s);
             int inscritos = 0;
             int salacap = 0;
             String[] stringarr = s.split(" ");
-            for (String word : stringarr){
+            for (String word : stringarr) {
                 if (inscritos == 0 && word.matches("\\b\\d+\\b")) {
                     inscritos = Integer.parseInt(word);
-                    System.out.println(inscritos);
+                    //System.out.println(inscritos);
                 } else if (salacap == 0 && inscritos > 0 && word.matches("\\b\\d+\\b")) {
                     salacap = Integer.parseInt(word);
-                    System.out.println(salacap);
+                    //System.out.println(salacap);
                 }
             }
             if (inscritos > salacap && salacap != 0)
@@ -31,15 +40,20 @@ public class SobrelotacaoHorario {
         return lista;
     }
 
-    public static int getNrSobreLotacoes(List<CalendarEvent> input){
+    /**
+     * Devolve o número de eventos sobrelotados 
+     *
+     * @param input lista de CalendarEvents 
+     * @return o total de eventos sobrelotados
+     */
+    public static int getNrSobreLotacoes(List<CalendarEvent> input) {
         return getSobrelotacoes(input).size();
     }
 
-
-    public static void main(String[] args) throws ParseException, IOException{
+    public static void main(String[] args) throws ParseException, IOException {
         List<CalendarEvent> eventos = showCSV.showHorario(new File("output.csv"));
-        List<CalendarEvent> xd = getSobrelotacoes(eventos);
-        System.out.println(getNrSobreLotacoes(xd));
+        List<CalendarEvent> sobreLot = getSobrelotacoes(eventos);
+        int nr = getNrSobreLotacoes(eventos);
+        System.out.println(nr);
     }
 }
-
