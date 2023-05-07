77a535bbc08fb2ae4cecd48b45c91043b8c44392
DiogoPeng <dbpgo@iscte-iul.pt>
2023-05-07 18:00:58 +0100

    SobrePosHr test+correcoes

--- src/test/java/pt/iscte_iul/ista/ES/SobrePosTeste.java
+++ src/test/java/pt/iscte_iul/ista/ES/SobrePosTeste.java
@@ -1,6 +1,10 @@
 package pt.iscte_iul.ista.ES;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -8,31 +12,24 @@ import java.util.Map;
 import org.junit.jupiter.api.Test;
 import Timetable.CalendarEvent;
 import Timetable.SobreposicaoHorario;
+import Timetable.showCSV;
 
 public class SobrePosTeste {
 
     @Test
-    public void testSobreposHorario() {
-        List<CalendarEvent> eventos = new ArrayList<>();
-        CalendarEvent a = new CalendarEvent("Evento 1", "Descrição do evento 1",
-                new Date(2023, 5, 10, 14, 0), new Date(2023, 5, 10, 16, 0));
-        eventos.add(a);
-        CalendarEvent b = new CalendarEvent("Evento 2", "Descrição do evento 2",
-                new Date(2023, 5, 10, 15, 0), new Date(2023, 5, 10, 17, 0));
-        eventos.add(b);
-        CalendarEvent c = new CalendarEvent("Evento 3", "Descrição do evento 3",
-                new Date(2023, 5, 10, 16, 0), new Date(2023, 5, 10, 18, 0));
-        eventos.add(c);
+    public void testSobreposHorario() throws ParseException, IOException {
 
-        Map<Date, List<CalendarEvent>> mapa = SobreposicaoHorario.SobreposHorario(eventos);
-
-        assertEquals(1, mapa.size());
-
-        Date dataSobreposicao = new Date(2023, 5, 10);
-        List<CalendarEvent> eventosSobrepostos = mapa.get(dataSobreposicao);
-        assertEquals(3, eventosSobrepostos.size());
-        assertEquals(true, eventosSobrepostos.get(0).getIsSobreposto());
-        assertEquals(true, eventosSobrepostos.get(1).getIsSobreposto());
-        assertEquals(true, eventosSobrepostos.get(2).getIsSobreposto());
-    }
+                List<CalendarEvent> eventos = showCSV.showHorario(
+                        new File("C:/Users/Utilizador/Desktop/ES2.0/ES-2023-2Sem-Ter-a-Feira-LEI-GrupoA/UTestShowCSVFen.csv"));
+                
+                CalendarEvent a = eventos.get(0);
+                CalendarEvent b = eventos.get(1);
+                CalendarEvent c = eventos.get(2);
+                Map<CalendarEvent, List<CalendarEvent>> mapa = SobreposicaoHorario.SobreposHorario(eventos);
+          
+                assertEquals(mapa.containsKey(a), false);
+                assertEquals(mapa.containsKey(b), true);
+                assertEquals(mapa.containsKey(c), true);
+        
+            }
 }
